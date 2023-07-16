604ab09827cbc3f7292946c6c3bf938fbf6f84a7
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-07-17 00:05:34 +0530

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -20,7 +20,7 @@ async def start_msg(c,m):
        await m.reply_text(
             text=Translation.START_TEXT,
             quote=True, 
-            reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton('⋆ Support ⋆', url=f"https://t.me/WizardBotHelper"),InlineKeyboardButton('⋆ 𝙾𝚠𝚗𝚎𝚛 ⋆', url="https://t.me/{OWNER_USERNAME}")]]))
+            reply_markup= InlineKeyboardMarkup([[InlineKeyboardButton('⋆ Support ⋆', url=f"https://t.me/WizardBotHelper"),InlineKeyboardButton('⋆ 𝙾𝚠𝚗𝚎𝚛 ⋆', url="https://t.me/{Config.OWNER_USERNAME}")]]))
     except Exception as e:
         log.error(str(e))
 
