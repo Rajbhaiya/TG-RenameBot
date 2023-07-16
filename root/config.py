b38771586897328733360a9cec04d042ed8f9f6e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-07-17 02:20:15 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -13,7 +13,7 @@ class Config(object):
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "cant_think_1")
     APP_ID = int(os.environ.get("APP_ID", "13675555"))
     API_HASH = os.environ.get("API_HASH", "c0da9c346d2c45dbc7ec49a05da9b2b6")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6113104447:AAHHE56nNSAQEr6KBbCN4vNEwNjki_8q7ZY")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6113104447:AAGwSAooj9xqJ_-apmmhZmM6gSzoR_mYkjQ")
     AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "5591954930").split()]
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./bot/DOWNLOADS")
     DB_URI = os.environ.get("DATABASE_URL", "postgres://kkktawwp:c9JreackKuiGxphwp0jDXlPilw4SFKLv@mouse.db.elephantsql.com/kkktawwp")
