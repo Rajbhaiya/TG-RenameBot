562ab66855353c8c3e7e6f20db21103fe61b6920
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-07-17 13:37:57 +0530

    Update bcast.py

--- root/plugins/bcast.py
+++ root/plugins/bcast.py
@@ -8,11 +8,11 @@ log = logging.getLogger(__name__)
     
 @Client.on_message(filters.command("broadcast") & filters.user(Config.OWNER_ID))
 async def bcast(c, m):
-    if message.reply_to_message:
-        MSG = message.reply_to_message
+    if m.reply_to_message:
+        MSG = m.reply_to_message
     else:
-        return await message.reply_text("First send me the message that you want to send to the other users of this bot! <b>Then as a reply to it send <code>/broadcast</code></b>")
-    m = await message.reply_text("`Broadcasting..`")
+        return await m.reply_text("First send me the message that you want to send to the other users of this bot! <b>Then as a reply to it send <code>/broadcast</code></b>")
+    m = await m.reply_text("`Broadcasting..`")
     ALLCHATS = get_all_chats()
     SUCE = 0
     FAIL = 0
@@ -30,6 +30,6 @@ async def bcast(c, m):
     if FAIL > 0:
       await m.edit_text("Generating Error Report !")
       open("ErrorReport.txt", "w").write(STR)
-      await message.reply_document("ErrorReport.txt", caption="Errors on Broadcast")
+      await m.reply_document("ErrorReport.txt", caption="Errors on Broadcast")
       os.remove("ErrorReport.txt")
     await m.delete()
