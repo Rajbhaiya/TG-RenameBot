ee14f803bd6a3bc033d8f879ce26f9c131ee9d89
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-07-17 15:00:31 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -16,7 +16,7 @@ class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6113104447:AAGDJohjeZixKlc1PTUXe8cdEoOh_Xkexe8")
     AUTH_USERS = [int(x) for x in os.environ.get("AUTH_USERS", "5591954930").split()]
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./bot/DOWNLOADS")
-    DB_URI = os.environ.get("DATABASE_URL", "postgres://kkktawwp:c9JreackKuiGxphwp0jDXlPilw4SFKLv@mouse.db.elephantsql.com/kkktawwp")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://vyjeeprz:QZJFgX_Q5WXo81HDWoDMnYgMz2nvfRQW@snuffleupagus.db.elephantsql.com/vyjeeprz")
     # owner is for log cmd only owner can use (this can be multiple users)
     OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "5591954930")]
     FORCEJOIN = os.environ.get("FORCEJOIN", "Wizard_Bots")
